18240c5e064b7424c90c8286e3a49b4f1e51f212
Talleyman <stephen522@earthlink.net>
2015-04-06 00:59:02 -0400

    Added SVG outputs

--- Metagenomic_summaries.R
+++ Metagenomic_summaries.R
@@ -27,7 +27,7 @@ outnames<-paste(paste("Summary Plots", data.names, sep="-"),".pdf",sep="")
 #Outnames not working in pdf file, so for now have to enter file names manually...
 #Will look into this for future use
 #For now, just change the index of i and the name to the outputs manually
-i<-12
+i<-1
 pdf("Summary Plots-Dataset Phyllo_Genus.cts_75 (method 2).pdf")
 ggplot(analyses[[i]], aes(x=analyses[[i]]$mean_group1,y=analyses[[i]]$effect, colour=analyses[[i]]$effect))+geom_point(size=3.5)+
   labs(x="Mean: Watered Group", y="Effect Size")+scale_colour_gradient("Effect",low="green", high="blue")+
@@ -39,6 +39,28 @@ ggplot(analyses[[i]], aes(x=analyses[[i]]$mean_group2,y=analyses[[i]]$effect, co
   ggtitle(expression(atop("Mean of Drought Group by Effect Size")))+scale_x_reverse()+
   theme(plot.title = element_text(size=20, face="bold", colour="black", vjust=-1), axis.title=element_text(size=14))
   
+ggplot(analyses[[i]], aes(x=analyses[[i]]$effect,y=-log10(analyses[[i]]$p.adj), colour=-log10(analyses[[i]]$p.adj)))+geom_point(size=3.5)+
+  labs(x="Effect Size", y="-log10(p-value)")+scale_colour_gradientn("Transformed\np-value",colours=rainbow(5))+
+  ggtitle(expression(atop("Effect Size by -log10 Adjusted p-value")))+scale_x_reverse()+
+  theme(plot.title = element_text(size=20, face="bold", colour="black", vjust=-1), axis.title=element_text(size=14))
+dev.off()
+
+#Higher res SVG plots
+svg("Watered by Effect-Dataset ECIDs.cts_75.svg")
+ggplot(analyses[[i]], aes(x=analyses[[i]]$mean_group1,y=analyses[[i]]$effect, colour=analyses[[i]]$effect))+geom_point(size=3.5)+
+  labs(x="Mean: Watered Group", y="Effect Size")+scale_colour_gradient("Effect",low="green", high="blue")+
+  ggtitle(expression(atop("Mean of Watered Group by Effect Size")))+scale_x_reverse()+
+  theme(plot.title = element_text(size=20, face="bold", colour="black", vjust=-1), axis.title=element_text(size=14))
+dev.off()
+
+svg("Drought by Effect-Dataset ECIDs.cts_75.svg")
+ggplot(analyses[[i]], aes(x=analyses[[i]]$mean_group2,y=analyses[[i]]$effect, colour=analyses[[i]]$effect))+geom_point(size=3.5)+
+  labs(x="Mean: Drought Group", y="Effect Size")+scale_colour_gradient("Effect",low="red", high="yellow")+
+  ggtitle(expression(atop("Mean of Drought Group by Effect Size")))+scale_x_reverse()+
+  theme(plot.title = element_text(size=20, face="bold", colour="black", vjust=-1), axis.title=element_text(size=14))
+dev.off()
+
+svg("Effect by Pvalue-Dataset ECIDs.cts_75.svg")
 ggplot(analyses[[i]], aes(x=analyses[[i]]$effect,y=-log10(analyses[[i]]$p.adj), colour=-log10(analyses[[i]]$p.adj)))+geom_point(size=3.5)+
   labs(x="Effect Size", y="-log10(p-value)")+scale_colour_gradientn("Transformed\np-value",colours=rainbow(5))+
   ggtitle(expression(atop("Effect Size by -log10 Adjusted p-value")))+scale_x_reverse()+
